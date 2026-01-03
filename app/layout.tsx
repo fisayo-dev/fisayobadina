13d895119db50acc5a1cc2af0932cfe3676caacf
fisayo-dev <olufisayobadina@gmail.com>
2026-01-03 23:25:27 +0100

    feat: integrate vercel wen analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const spaceGrotesk = localFont({
   src: "../public/fonts/SpaceGrotesk-Regular.ttf",
@@ -83,6 +84,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={`${spaceGrotesk.variable} antialiased`}>
         <div className="max-w-4xl mx-auto px-5 lg:px-20 py-20"> {children}</div>
+        <Analytics />
       </body>
     </html>
   );
